56d90fe46fc43313eee6ea5249fa89ed02dfa417
adistar01 <aditya21072000@gmail.com>
2022-07-27 13:45:54 +0530

    pysimplegui

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
+from lib2to3.pgen2.tokenize import generate_tokens
 import os
 import sys
 import subprocess
 
 #TEST_CONFIG_JSON = "config.json"
 #TEST_IMAGE = "testP1.png"
-
+from wifi_heat_mapper.graph import generate_graph
 class MakeHeatMap:
     def __init__(self,TEST_IMAGE, TEST_CONFIG_JSON):
         self.img = TEST_IMAGE
         self.json = TEST_CONFIG_JSON
 
     def run(self):
-        v = True
-        #pid = os.system(
-        #    "whm plot --map ./images/" + self.img + " --config " + self.json
-        #    )
-        os.system(
-            "whm plot --map ./images/" + self.img + " --config " + self.json
-            )
-        #if pid==0:
-        #   return
-        #subprocess.call("whm plot --map ./images/" + self.img + " --config " + self.json, shell=True)
+        generate_graph(data=self.json, floor_map="./images/" + self.img)
+        
+        # os.system(
+        #     "whm plot --map ./images/" + self.img + " --config " + self.json
+        #     )
 
 
 if __name__ == "__main__":
